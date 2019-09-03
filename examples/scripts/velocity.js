f6af62fbf805cf8e956d92b44517980545352303
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2019-09-03 15:09:52 +0530

    Added remaining colors to coloring.

--- examples/scripts/velocity.js
+++ examples/scripts/velocity.js
@@ -1,5 +1,5 @@
 /**
- * Velocity - v1.0.0-alpha1 - 2019-08-30
+ * Velocity - v1.0.0-alpha1 - 2019-09-03
  * Description: Velocity is a JavaScript library which provide utilities, ui components and MVC framework implementation.
  * License: GPL-3.0-or-later
  * Author: Bhagwat Singh Chouhan
@@ -3161,6 +3161,105 @@ cmt.components.jquery = cmt.components.jquery || {};
 	// Default Settings
 	cmtjq.fn.cmtCheckbox.defaults = {
 		// options
+	};
+
+})( jQuery );
+
+
+/**
+ * The Counter Widget increment or decrement numerical value of a field.
+ */
+
+( function( cmtjq ) {
+
+// TODO: Add option for multi select
+
+	cmtjq.fn.cmtCounter = function( options ) {
+
+		// == Init == //
+
+		// Configure Plugin
+		var settings	= cmtjq.extend( {}, cmtjq.fn.cmtCounter.defaults, options );
+		var counters	= this;
+
+		// Iterate and initialise all the fox sliders
+		counters.each( function() {
+
+			var counter = cmtjq( this );
+
+			init( counter );
+		});
+
+		// return control
+		return;
+
+		// == Private Functions == //
+
+		function init( counter ) {
+
+			var min		= cmt.utils.data.hasAttribute( counter, 'data-min' ) ? counter.attr( 'data-min' ) : settings.min;
+			var max		= cmt.utils.data.hasAttribute( counter, 'data-max' ) ? counter.attr( 'data-max' ) : settings.max;
+			var val		= cmt.utils.data.hasAttribute( counter, 'data-val' ) ? counter.attr( 'data-val' ) : settings.val;
+			var cval	= val;
+
+			var incBtn	= counter.find( '.counter-inc' );
+			var decBtn	= counter.find( '.counter-dec' );
+			var field	= counter.find( '.counter-val' );
+
+			// Set value
+			field.val( cval );
+
+			incBtn.click( function() {
+
+				cval = field.val( cval );
+
+				if( cval < max ) {
+
+					cval++;
+
+					field.val( cval );
+
+					if( cval >= max ) {
+
+						incBtn.addClass( 'disabled' );
+
+					}
+					else {
+
+						incBtn.removeClass( 'disabled' );
+					}
+				}
+			});
+
+			decBtn.click( function() {
+
+				cval = field.val( cval );
+
+				if( cval > min ) {
+
+					cval--;
+
+					field.val( cval );
+
+					if( cval <= min ) {
+
+						decBtn.addClass( 'disabled' );
+
+					}
+					else {
+
+						decBtn.removeClass( 'disabled' );
+					}
+				}
+			});
+		}
+	};
+
+	// Default Settings
+	cmtjq.fn.cmtCounter.defaults = {
+		min: 0,
+		max: 10,
+		val: 0
 	};
 
 })( jQuery );
