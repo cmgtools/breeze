b60bd63f871af480d921ca5ae3aa7441ff1a1a74
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2020-04-24 21:39:48 +0530

    Applied quick fixes.

--- examples/scripts/velocity.js
+++ examples/scripts/velocity.js
@@ -1,5 +1,5 @@
 /**
- * Velocity - v1.0.0-alpha1 - 2020-04-10
+ * Velocity - v1.0.0-alpha1 - 2020-04-24
  * Description: Velocity is a JavaScript library which provide utilities, ui components and MVC framework implementation.
  * License: GPL-3.0-or-later
  * Author: Bhagwat Singh Chouhan
@@ -3268,6 +3268,71 @@ cmt.components.jquery = cmt.components.jquery || {};
 	// Default Settings
 	cmtjq.fn.cmtCheckbox.defaults = {
 		// options
+	};
+
+})( jQuery );
+
+
+/**
+ * Collapsible plugin can be used to keep the title visible, but show or hide content.
+ */
+
+( function( cmtjq ) {
+
+	cmtjq.fn.cmtCollapsible = function( options ) {
+
+		// == Init == //
+
+		// Configure Plugin
+		var settings		= cmtjq.extend( {}, cmtjq.fn.cmtCollapsible.defaults, options );
+		var collapsibles	= this;
+
+		// Iterate and initialise all the collapsibles
+		collapsibles.each( function() {
+
+			var collapsible = cmtjq( this );
+
+			init( collapsible );
+		});
+
+		// return control
+		return;
+
+		// == Private Functions == //
+
+		function init( collapsible ) {
+
+			var trigger	= collapsible.find( '.cmt-collapsible-trigger' );
+			var header	= collapsible.find( '.cmt-collapsible-header' );
+			var view	= collapsible.find( '.cmt-collapsible-content' );
+
+			// Hide View
+			if( settings.hideView ) {
+
+				view.hide();
+			}
+
+			// Listen click
+			trigger.click( function() {
+
+				view.slideToggle();
+			});
+
+			// Header Trigger
+			if( settings.headerTrigger ) {
+
+				header.click( function() {
+
+					view.slideToggle();
+				});
+			}
+		}
+	};
+
+	// Default Settings
+	cmtjq.fn.cmtCollapsible.defaults = {
+		hideView: true,
+		headerTrigger: true
 	};
 
 })( jQuery );
